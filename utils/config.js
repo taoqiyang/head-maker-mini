086b341f198334582ae5070467561e02d1a89a16
草貌路飞 <yang0qi0tao0@gmail.com>
2023-01-05 17:27:35 +0800

    [M]修改在线资源地址

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   // 仅为临时测试地址，正式环境使用时，请务必更换！！！
-  staticUrl: 'https://serverless-api-delta.vercel.app/yi-tool-mini'
+  staticUrl: 'https://taoqiyang.oss-cn-beijing.aliyuncs.com/head-maker-mini'
 }
